951b992f7404d7898da7a80f3e34c19779f8a561
Kevin Zhang <kevin@kevinzhang.me>
2016-08-29 02:28:49 -0400

    Move to package structure and add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dottorrent",
+    version='1.0.0',
+    packages=find_packages(),
+    scripts=['dottorrent_gui/dottorrent_gui.py'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['dottorrent>=1.4.1'],
+
+    # metadata for upload to PyPI
+    author="Kevin Zhang",
+    author_email="kevin@kevinzhang.me",
+    description="An advanced GUI torrent file creator with batch functionality, powered by PyQt and dottorrent",
+    # long_description=open('README.rst').read(),
+    keywords="bittorrent torrent",
+    url="https://github.com/kz26/dottorrent-gui",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
